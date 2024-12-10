26d88c1ed2a1367bb3d8782142e9d37e277e7d51
Chaesunbak <coanstjd0422@gmail.com>
2024-12-10 23:20:47 +0900

    fix : 올바르게 린트 설정

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,46 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default {
+  ignores: ['dist'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    js.configs.recommended,
+  ],
+  files: ['**/*.{ts,tsx}'],
+  parser: tsParser,
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+  },
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    '@typescript-eslint': tseslint,
+    prettier: prettierPlugin,
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'no-multiple-empty-lines': 'error',
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-    },
-  }
-);
+  },
+};
