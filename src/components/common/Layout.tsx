4914951947991f95a8d28db1e21c32523a1022de
Chaesunbak <coanstjd0422@gmail.com>
2024-12-11 00:35:31 +0900

    fix : footer 컴포넌트 삭제

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,5 +1,5 @@
 import Header from './Header';
-import Footer from './Footer';
+
 import { Outlet } from 'react-router';
 
 const Layout = () => {
@@ -7,7 +7,6 @@ const Layout = () => {
     <div className="mx-auto flex min-h-screen flex-col lg:max-w-6xl">
       <Header />
       <Outlet />
-      <Footer />
     </div>
   );
 };
