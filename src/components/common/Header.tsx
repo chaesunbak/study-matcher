ced3cf9da129125adc9a711699dbbcce68058b7b
Chaesunbak <coanstjd0422@gmail.com>
2024-12-11 00:30:20 +0900

    feat : 헤더 컴포넌트 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,23 @@
+import { Link } from 'react-router';
+import { LuSearch, LuCircleUserRound, LuAlignJustify } from 'react-icons/lu';
+import SearchInput from '../SearchInput';
+import { useNavigate } from 'react-router';
+
 const Header = () => {
-	return (
-		<header>
-			<h1>Header</h1>
-		</header>
-	);
+  const navigate = useNavigate();
+  return (
+    <header className="flex items-center justify-between p-4">
+      <Link to="/">
+        <h1 className="text-4xl font-bold">LOGO</h1>
+      </Link>
+      <SearchInput className="hidden md:flex" />
+      <div className="flex gap-4">
+        <LuSearch className="size-6 cursor-pointer md:hidden" onClick={() => navigate('/search')} />
+        <LuCircleUserRound className="size-6 cursor-pointer" />
+        <LuAlignJustify className="size-6 cursor-pointer" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
