b14600d3f72e59551c78a9f921ab2ed4683840e6
Chaesunbak <coanstjd0422@gmail.com>
2024-12-13 00:45:57 +0900

    feat : 글로벌 스타일 적용

--- src/components/GroupSummary.tsx
+++ src/components/GroupSummary.tsx
@@ -14,8 +14,8 @@ const GroupSummary = ({ meeting }: MeetingSummaryProps) => {
         {/* <img src={meeting.imgUrl} alt={meeting.title} /> */}
       </div>
       <div className="flex h-full flex-col gap-2">
-        <h3 className="font-semibold md:text-xl">{meeting.title}</h3>
-        <p className="md:text-md text-sm font-thin lg:text-base">{meeting.description}</p>
+        <h3>{meeting.title}</h3>
+        <p>{meeting.description}</p>
         <div className="flex items-center justify-center text-sm font-thin">
           <span className="flex items-center text-gray-500">
             <LuCalendar />
