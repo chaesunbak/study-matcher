b2f60a9f20f1e54f233bb2d2de7ccad2059a25ef
Chaesunbak <coanstjd0422@gmail.com>
2024-12-12 18:46:11 +0900

    fix : 레이아웃 변경

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router';
 
 const Layout = () => {
   return (
-    <div className="mx-auto flex min-h-screen flex-col lg:max-w-6xl">
+    <div className="container mx-auto flex min-h-screen flex-col px-4">
       <Header />
-      <main className="p-4">
+      <main>
         <Outlet />
       </main>
     </div>
