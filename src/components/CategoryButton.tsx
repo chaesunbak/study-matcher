4e803bd30624174ae915d770433f59205a8ba101
Chaesunbak <coanstjd0422@gmail.com>
2024-12-13 14:58:08 +0900

    fix : 카테고리 버튼 색상 변경

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -11,10 +11,10 @@ const CategoryButton = ({ category, onClick, isActive = false }: CategoryButtonP
     <button
       onClick={onClick}
       className={clsx(
-        'rounded-full px-2 py-1 text-sm transition-all',
+        'rounded-full border px-2 py-1 text-sm transition-all',
         isActive
-          ? 'bg-primary border-primary'
-          : 'border-gray-500 bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700'
+          ? 'bg-primary border-primary font-medium text-white'
+          : 'bg-gray-100 hover:bg-gray-200 dark:bg-gray-800 dark:hover:bg-gray-700'
       )}
     >
       {category}
