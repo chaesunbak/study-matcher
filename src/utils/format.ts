eacdb5ca716fd32320266410d3e583e9c31f40a3
layout-SY <boycho2000@naver.com>
2024-12-17 22:21:08 +0900

    add : 포맷 함수 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -11,3 +11,13 @@ export const formatDate = (date: Date): string => {
 
   return `${year}년 ${month}월 ${day}일`;
 };
+
+export const formatDateYyyyMmDd = (dateString: Date) => {
+  const date = new Date(dateString);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+};
