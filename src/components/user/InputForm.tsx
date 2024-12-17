f19e903762f03a8ebf3bbe9b76220a9884ca640b
layout-SY <boycho2000@naver.com>
2024-12-17 22:20:55 +0900

    fix : Input 폼 일부 수정

--- src/components/user/InputForm.tsx
+++ src/components/user/InputForm.tsx
@@ -8,7 +8,7 @@ type Option = {
 type InputProps = {
   control: Control<any>;
   name: string;
-  placeholder: string;
+  placeholder?: string;
   errors: FieldErrors;
   type?: 'text' | 'password' | 'textarea' | 'select' | 'date';
   className?: string;
@@ -18,20 +18,21 @@ type InputProps = {
 const Input = ({
   control,
   name,
-  placeholder,
   errors,
+  placeholder = '',
   type = 'text',
   className = '',
   options = [],
 }: InputProps) => {
-  const hasError = Boolean(errors?.[name]); // 에러 존재 여부
+  const hasError = Boolean(errors?.[name]);
+
+  // 기본 스타일 정의
+  const basicStyle = `focus:outline-none focus:ring-2
+  ${hasError ? 'border-red-500 focus:ring-red-500' : 'focus:ring-indigo-500'}
+  ${className}`;
 
   // 공통 스타일
-  const baseClass = `
-    w-full rounded-lg border px-4 py-2 text-gray-700 focus:outline-none focus:ring-2
-    ${hasError ? 'border-red-500 focus:ring-red-500' : 'focus:ring-indigo-500'}
-    ${className}
-  `;
+  const baseClass = `w-full rounded-lg border px-4 py-2 text-gray-700 ${basicStyle}`;
 
   const renderInput = (field: any) => {
     switch (type) {
