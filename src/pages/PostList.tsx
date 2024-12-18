0b92ac424cc69152936883524255f696416d2210
Chaesunbak <coanstjd0422@gmail.com>
2024-12-19 00:02:40 +0900

    fix : 불필요한 코드 제거

--- src/pages/PostList.tsx
+++ src/pages/PostList.tsx
@@ -1,18 +1,9 @@
-// import { useParams } from 'react-router';
-import { useOutletContext } from 'react-router';
 import GroupPostSection from '../components/groupdetail/GroupPostSection';
-import { dummyPosts } from '../data';
-
-interface ContextType {
-  posts: typeof dummyPosts;
-}
 
 const PostList = () => {
-  // const { group_id } = useParams();
-  const { posts } = useOutletContext<ContextType>();
   return (
     <div>
-      <GroupPostSection posts={posts} />
+      <GroupPostSection />
     </div>
   );
 };
