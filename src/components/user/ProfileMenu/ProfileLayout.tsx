9b29c51caf6f1fb0d8f3249318f3b53aec9c0e3e
layout-SY <boycho2000@naver.com>
2024-12-18 23:53:37 +0900

    feat : user 데이터 Link 시 state로 가져오게 변경

--- src/components/user/ProfileMenu/ProfileLayout.tsx
+++ src/components/user/ProfileMenu/ProfileLayout.tsx
@@ -1,15 +1,14 @@
-import { Outlet, useParams } from 'react-router';
+import { Outlet } from 'react-router';
 import UserHeaderForm from '../UserHeaderForm';
 
 const ProfileLayout = () => {
-  const user_id = useParams<{ user_id: string }>();
   return (
     <div className="bg-gray-50 flex min-h-screen">
       <UserHeaderForm mode="leftMenu" />
 
       {/* 오른쪽 콘텐츠 */}
       <div className="w-3/4 p-6">
-        <Outlet context={{ user_id }} />
+        <Outlet />
       </div>
     </div>
   );
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,9 @@
-import { useOutletContext, useParams } from 'react-router';
 import ProfileForm from '../components/user/ProfileMenu/ProfileForm';
 
-interface ContextType {
-  user_id: string;
-}
-
 const Profile = () => {
-  const { user_id } = useOutletContext<ContextType>();
   return (
     <div>
-      <ProfileForm user_id={user_id} />
+      <ProfileForm />
     </div>
   );
 };
