3d0ad90767d8aa6842c86e57a5d768a8622a54f3
layout-SY <boycho2000@naver.com>
2024-12-18 23:53:56 +0900

    add : 새로운 user 타입 추가

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,19 @@
-type Gender = 'male' | 'female';
+type Gender = '남성' | '여성';
 
 export interface User {
-  id: number;
   email: string;
   username: string;
   gender: Gender;
   birth_date: string;
   profile_img: string;
   introduction: string;
+}
+
+export interface UserWithPassword extends User {
+  password: string;
+}
+
+export interface UserResponse extends User {
+  id: number;
   created_at: string;
 }
