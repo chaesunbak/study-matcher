30fe6cf14a717230b0fb34934c06a7a3039c7a67
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2024-12-19 18:59:12 +0900

    feat : Route 추가 및 수정

--- src/main.tsx
+++ src/main.tsx
@@ -16,11 +16,12 @@ import GroupCreate from './pages/GroupCreate.tsx';
 import PostList from './pages/PostList.tsx';
 import PostDetail from './pages/PostDetail.tsx';
 import PostWrite from './pages/PostWrite.tsx';
-import ForgotPw from './pages/ForgotPw.tsx';
 import GroupManageList from './pages/GroupManageList.tsx';
 import Logout from './pages/Logout.tsx';
 import UserModify from './pages/UserModify.tsx';
 import ProfileLayout from './components/user/\bProfileMenu/ProfileLayout.tsx';
+import ResetPw from './pages/ResetPw.tsx';
+import WithdrawUser from './pages/WithdrawUser.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -32,12 +33,13 @@ createRoot(document.getElementById('root')!).render(
           {/* 유저관련 */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/forget-password" element={<ForgotPw />} />
           <Route path="/users/:user_id" element={<ProfileLayout />}>
             <Route index element={<Profile />} />
             <Route path="modify" element={<UserModify />} />
             <Route path="manage" element={<GroupManageList />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="reset-password" element={<ResetPw />} />
+            <Route path="withdraw" element={<WithdrawUser />} />
           </Route>
           {/* 그룹관련 */}
           <Route path="/groups/:group_id" element={<GroupLayout />}>
