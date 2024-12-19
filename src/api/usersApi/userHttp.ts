5d6c6b50b39916e36ab3428a77013956e0888d8e
Cho SeungYeon <111514472+layout-SY@users.noreply.github.com>
2024-12-19 12:18:18 +0900

    feat : 사용자 API fetching axios 추가

---
+++ src/api/usersApi/userHttp.ts
@@ -0,0 +1,73 @@
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const DEFAULT_TIMEOUT = 30000;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: DEFAULT_TIMEOUT,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    if (config.method !== 'post') {
+      const token = sessionStorage.getItem('access_token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      //   try {
+      //     const newAccessToken = await refreshAccessToken();
+      //     error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+      //     return axiosInstance.request(error.config);
+      //   } catch (refreshError) {
+      //     console.error('리프레시 중 에러 발생:', refreshError);
+      //     return Promise.reject(refreshError);
+      //   }
+      window.location.href = '/login';
+      return;
+    }
+    return Promise.reject(error);
+  }
+);
+
+type RequestMethod = 'get' | 'post' | 'put' | 'delete';
+
+export const requestHandlerUser = async <T, U = any>(
+  method: RequestMethod,
+  url: string,
+  payload?: T
+): Promise<U> => {
+  switch (method) {
+    case 'get':
+      return await axiosInstance.get(url);
+
+    case 'post':
+      return await axiosInstance.post(url, payload ?? { default: 'default_value' });
+
+    case 'put':
+      return await axiosInstance.put(url, payload);
+
+    case 'delete':
+      return await axiosInstance.delete(url);
+
+    default:
+      throw new Error('Unsupported request method');
+  }
+};
