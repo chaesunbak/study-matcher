507a4b082e492ff94360ebbbc21b2717bd46ce55
Chaesunbak <coanstjd0422@gmail.com>
2024-12-20 21:36:59 +0900

    fix : 그룹 서머리에서 더 많은 정보 제공

--- src/components/GroupSummary.tsx
+++ src/components/GroupSummary.tsx
@@ -14,18 +14,32 @@ const GroupSummary = ({ meeting }: MeetingSummaryProps) => {
         {/* TDOO: 현재는 스켈레톤을 띄우고 있으나 나중에 실제 이미지로 변경 */}
         {/* <img src={meeting.imgUrl} alt={meeting.title} /> */}
       </div>
-      <div className="flex h-full flex-col gap-2">
+      <div className="flex h-full flex-col items-start justify-between">
         <h3 className="hover:underline">{meeting.title}</h3>
         <p>{meeting.description}</p>
         <div className="flex items-center gap-2 text-sm font-thin text-gray-500">
           <span className="flex items-center gap-1">
             <LuCalendar />
-            {new Date(meeting.created_at).toLocaleDateString()}
+            생성 {new Date(meeting.created_at).toLocaleDateString()}
           </span>
-          <span className="flex items-center gap-1">
-            <LuUser />
-            {15}
-          </span>
+          {meeting.max_members && (
+            <span className="flex items-center gap-1">
+              <LuUser />
+              정원{meeting.max_members}명
+            </span>
+          )}
+          {meeting.start_date && (
+            <span className="flex items-center gap-1">
+              <LuCalendar />
+              시작 {new Date(meeting.start_date).toLocaleDateString()}
+            </span>
+          )}
+          {meeting.end_date && (
+            <span className="flex items-center gap-1">
+              <LuCalendar />
+              시작 {new Date(meeting.end_date).toLocaleDateString()}
+            </span>
+          )}
         </div>
       </div>
     </Link>
