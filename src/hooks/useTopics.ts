d2dc2e15f90aca324dfa27545a89ff608bb7d7ef
Chaesunbak <coanstjd0422@gmail.com>
2024-12-23 00:06:52 +0900

    feat : useTopic에 리액트 쿼리 적용해서 캐싱 적용

--- src/hooks/useTopics.ts
+++ src/hooks/useTopics.ts
@@ -1,30 +1,22 @@
-import { useState, useEffect } from 'react';
 import { Topic } from '../models/topic.model';
 import { getTopics } from '../api/topcis.api';
+import { useQuery } from '@tanstack/react-query';
 
-// TODO : 후에 리액트 쿼리를 활용하여 캐시
 const useTopics = () => {
-  const [topics, setTopics] = useState<Topic[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  // useQuery 훅을 사용하여 토픽 데이터를 가져옵니다.
+  const {
+    data: topics,
+    isLoading: loading,
+    error,
+  } = useQuery<Topic[], Error>({
+    queryKey: ['topics'], // 쿼리 키를 설정합니다.
+    queryFn: getTopics, // 데이터를 가져오는 함수입니다.
+    staleTime: Infinity, // 데이터가 오래된 것으로 간주되지 않도록 설정합니다.
+    gcTime: Infinity, // 캐시된 데이터가 무기한 유지되도록 설정합니다.
+  });
 
-  useEffect(() => {
-    const fetchTopics = async () => {
-      try {
-        const topics = await getTopics();
-        setTopics(topics);
-      } catch (error) {
-        console.error(error);
-        setError('Failed to fetch topics');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchTopics();
-  }, []);
-
-  return { topics, loading, error };
+  // 토픽 데이터, 로딩 상태, 에러 메시지를 반환합니다.
+  return { topics: topics || [], loading, error: error ? error.message : null };
 };
 
 export default useTopics;
